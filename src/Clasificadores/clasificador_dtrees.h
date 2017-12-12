9626e73198f32f4996c1c1b8e3596ec9d8126840
isanroman <isanromanlana@gmail.com>
2017-12-12 21:16:14 +0100

    Solved Issue-#5-CVFolds-error-when-starting

--- src/Clasificadores/clasificador_dtrees.h
+++ src/Clasificadores/clasificador_dtrees.h
@@ -32,7 +32,7 @@ using namespace cv;
 namespace MLT {
     class Clasificador_DTrees: public Clasificador{
     public:
-        Clasificador_DTrees(string Nombre="",int max_depth=INT_MAX, int min_sample_count=10, float regression_accuracy=0.01, bool use_surrogates=true, int max_categories=10, int cv_folds=10, bool use_1se_rule=true, bool truncate_pruned_tree=true, Mat priors=Mat());
+        Clasificador_DTrees(string Nombre="",int max_depth=INT_MAX, int min_sample_count=10, float regression_accuracy=0.01, bool use_surrogates=true, int max_categories=10, int cv_folds=0, bool use_1se_rule=true, bool truncate_pruned_tree=true, Mat priors=Mat());
         ~Clasificador_DTrees();
         int Parametrizar(int max_depth, int min_sample_count, float regression_accuracy, bool use_surrogates, int max_categories, int cv_folds, bool use_1se_rule, bool truncate_pruned_tree, Mat priors);
         int Autotrain(vector<Mat> Data, vector<float> Labels, Dimensionalidad::Reducciones reduc, Generacion::Info_Datos info, bool save=true);
